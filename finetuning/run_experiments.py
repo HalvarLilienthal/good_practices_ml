21b4a09065a39fb9a0d4779a631e667ee999e7f3
Mathias Rasper <mrasper@uni-bremen.de>
2025-01-14 09:16:15 +0100

    Added Default

--- finetuning/run_experiments.py
+++ finetuning/run_experiments.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser(description='Pretrained Model')
     parser.add_argument('--yaml_path', metavar='str', required=True,
-                        help='The path to the yaml file with the stored paths')
+                        help='The path to the yaml file with the stored paths', default='../paths.yaml')
     parser.add_argument('-d', '--debug', action='store_true',
                         required=False, help='Enable debug mode', default=False)
     args = parser.parse_args()
